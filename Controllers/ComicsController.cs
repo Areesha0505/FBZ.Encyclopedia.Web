5233f65d59235713eb9f42a3fb9573bc8edd5f6c
Areesha0505 <areesha1678@gmail.com>
2026-03-08 16:04:46 +0000

    Fixed CSV parsing using TryParse for Year column

--- Controllers/ComicsController.cs
+++ Controllers/ComicsController.cs
@@ -1,27 +1,33 @@
-﻿using FBZ.Encyclopedia.Web.Models;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
+using FBZ.Encyclopedia.Web.Models;
 
-public IActionResult Index()
+namespace FBZ.Encyclopedia.Web.Controllers
 {
-    var comics = new List<ComicRecord>();
-
-    var titlesPath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/Data/titles.csv");
-    var recordsPath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/Data/records.csv");
-
-    var titleLines = System.IO.File.ReadAllLines(titlesPath).Skip(1).ToList();
-    var recordLines = System.IO.File.ReadAllLines(recordsPath).Skip(1).ToList();
-
-    for (int i = 0; i < titleLines.Count && i < recordLines.Count; i++)
+    public class ComicsController : Controller
     {
-        var titleParts = titleLines[i].Split(',');
-        var recordParts = recordLines[i].Split(',');
-
-        comics.Add(new ComicRecord
+        public IActionResult Index()
         {
-            Title = titleParts[1],
-            Year = int.Parse(recordParts[2])
-        });
-    }
+            var comics = new List<ComicRecord>();
 
-    return View(comics);
+            var titlesPath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/Data/titles.csv");
+            var recordsPath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/Data/records.csv");
+
+            var titleLines = System.IO.File.ReadAllLines(titlesPath).Skip(1).ToList();
+            var recordLines = System.IO.File.ReadAllLines(recordsPath).Skip(1).ToList();
+
+            for (int i = 0; i < titleLines.Count && i < recordLines.Count; i++)
+            {
+                var titleParts = titleLines[i].Split(',');
+                var recordParts = recordLines[i].Split(',');
+
+                comics.Add(new ComicRecord
+                {
+                    Title = titleParts[1],
+                    Year = int.TryParse(recordParts[2], out int year) ? year : 0
+                });
+            }
+
+            return View(comics);
+        }
+    }
 }
