b5524fc8221dc5d8c34031bf74d25bcfb0240ce2
Areesha0505 <areesha1678@gmail.com>
2026-03-08 15:21:28 +0000

    Initial ASP.NET MVC project setup for FBZ Encyclopedia

---
+++ Models/ComicRecord.cs
@@ -0,0 +1,30 @@
+﻿public class ComicRecord
+{
+    public string Title { get; set; }
+
+    public string OtherTitles { get; set; }
+
+    public string ResourceType { get; set; }
+
+    public string ContentType { get; set; }
+
+    public string MaterialType { get; set; }
+
+    public string ISBN { get; set; }
+
+    public string Name { get; set; }
+
+    public string DateOfPublication { get; set; }
+
+    public string Edition { get; set; }
+
+    public string PhysicalDescription { get; set; }
+
+    public string DeweyClassification { get; set; }
+
+    public string Topics { get; set; }
+
+    public string Genre { get; set; }
+
+    public string Language { get; set; }
+}
