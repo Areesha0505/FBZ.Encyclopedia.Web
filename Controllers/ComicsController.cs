12511671506e689705a90b9fccac904d70343b02
Areesha0505 <areesha1678@gmail.com>
2026-03-08 15:23:36 +0000

    Added ComicRecord model for FBZ dataset

---
+++ Controllers/ComicsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FBZ.Encyclopedia.Web.Controllers
+{
+    public class ComicsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
