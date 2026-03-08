ae3024de8da9673c258b8944c9079eae04d21eb9
Areesha0505 <areesha1678@gmail.com>
2026-03-08 15:45:23 +0000

    Load comic titles from titles.csv dataset

--- Models/ComicRecord.cs
+++ Models/ComicRecord.cs
@@ -1,11 +1,1 @@
-﻿namespace FBZ.Encyclopedia.Web.Models
-{
-    public class ComicRecord
-    {
-        public string Title { get; set; }
-
-        public string Publisher { get; set; }
-
-        public int Year { get; set; }
-    }
-}
+﻿Load comic titles from titles.csv dataset
